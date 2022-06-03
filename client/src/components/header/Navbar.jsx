516802f93e967836eb1a9f55aca5b1b09e9bbe99
Kalyani Sahu <sahu.kalyani879@gmail.com>
2022-06-03 08:08:37 +0530

    navbar

---
+++ client/src/components/header/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './navbar.css';
+import SearchIcon from '@mui/icons-material/Search'
+
+const Navbar = () => {
+  return (
+    <header>
+      <nav>
+        <div className="left">
+          <div className="navlogo">
+            <img src="./amazon_PNG25.png" alt="" />
+          </div>
+          <div className="nav_searchbar">
+            <input type="text" name='' id='' />
+            <div className="search_icon">
+              <SearchIcon/>
+            </div>
+          </div>
+        </div>
+        <div className="right"></div>
+      </nav>
+    </header>
+  )
+}
+
+export default Navbar
--- server/app.js
+++ server/app.js
@@ -1,1 +1,10 @@
-console.log("hello there")
+const express = require('express');
+require('dotenv').config();
+const app = express();
+
+const port = 8005;
+
+
+app.listen(port,()=>{
+    console.log(`server is running at ${port}`)
+})
